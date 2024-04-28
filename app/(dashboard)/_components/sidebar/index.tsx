bcc1bb4af4cdf0c0ec6fcc1e65b96b3ceb7b46d1
AkshatJha21 <akshatj2134@gmail.com>
2024-04-28 11:04:07 +0530

    sidebar component done

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,9 +1,11 @@
+import { List } from "./list";
 import { NewButton } from "./new-button";
 
 export const Sidebar = () => {
     return (
-        <aside className="fixed z-[1] left-0 bg-slate-800 text-white h-full w-[60px] flex p-3 flex-col gap-y-4">
+        <div className="fixed z-[1] left-0 bg-slate-800 text-white h-full w-[60px] flex p-3 flex-col gap-y-4">
+            <List />
             <NewButton />
-        </aside>
+        </div>
     );
 };
