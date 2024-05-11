0d17cc32d311d004a4f108c7f6a25490a9b69ad2
AkshatJha21 <akshatj2134@gmail.com>
2024-05-11 08:57:34 +0530

    link copy works

--- components/actions.tsx
+++ components/actions.tsx
@@ -8,6 +8,8 @@ import {
     DropdownMenuItem,
     DropdownMenuSeparator
 } from "@/components/ui/dropdown-menu";
+import { Link2 } from "lucide-react";
+import { toast } from "sonner";
 
 interface ActionProps {
     children: React.ReactNode;
@@ -24,12 +26,25 @@ export const Actions = ({
     id,
     title
 }: ActionProps) => {
+    const onCopyLink = () => {
+        navigator.clipboard.writeText(
+            `${window.location.origin}/lab/${id}`,
+        )
+        .then(() => toast.success("Link copied"))
+        .catch(() => toast.error("Failed to copy link"));
+    }
+
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 {children}
             </DropdownMenuTrigger>
-            
+            <DropdownMenuContent side={side} sideOffset={sideOffset} className="w-60" onClick={(e) => e.stopPropagation()}>
+                <DropdownMenuItem className="p-3 cursor-pointer" onClick={onCopyLink}>
+                    <Link2 className="h-4 w-4 mr-2"/>
+                    Copy Lab Link
+                </DropdownMenuItem>
+            </DropdownMenuContent>
         </DropdownMenu>
     )
 }
