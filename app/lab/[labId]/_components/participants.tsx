634982690d6b721ac54ebbaeb4ef7bd2af4d6e92
AkshatJha21 <akshatj2134@gmail.com>
2024-05-23 07:39:11 +0530

    room participants init

--- app/lab/[labId]/_components/participants.tsx
+++ app/lab/[labId]/_components/participants.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+"use client";
+
+import { UserAvatar } from "./user-avatar";
+import { useOthers, useSelf } from "@/liveblocks.config";
+
+const MAX_SHOWN_USERS = 2;
 
 export const Participants = () => {
+  const users = useOthers();
+  const currentUser = useSelf();
+  const hasMoreUsers = users.length > MAX_SHOWN_USERS;
+
   return (
     <div className='absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md'>
-        Users
+        <div className="flex gap-x-2">
+          {users.slice(0, MAX_SHOWN_USERS).map(({ connectionId, info }) => {
+            return (
+              <UserAvatar 
+                key={connectionId}
+                src={info?.picture}
+                name={info?.name}
+                fallback={info?.name?.[0] || "G"}
+              />
+            )
+          })}
+
+          {currentUser && (
+            <UserAvatar 
+              src={currentUser.info?.picture}
+              name={`${currentUser.info?.name} (You)`}
+              fallback={currentUser.info?.name?.[0]}
+            />
+          )}
+        </div>
     </div>
   )
 }
