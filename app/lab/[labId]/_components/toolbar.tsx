7e52de1ebea250a1daf1a3bcef50a3f613781e97
AkshatJha21 <akshatj2134@gmail.com>
2024-05-24 23:38:17 +0530

    top toolbar done

--- app/lab/[labId]/_components/toolbar.tsx
+++ app/lab/[labId]/_components/toolbar.tsx
@@ -1,25 +1,47 @@
-import { Pencil } from 'lucide-react'
+import { Circle, MousePointer2, Pencil, Square, StickyNote, Type } from 'lucide-react'
 import React from 'react'
+import { ToolButton } from './tool-button'
 
 export const ToolBar = () => {
   return (
     <div className='absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4'>
         <div className='bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md'>
-            <div>
-                <Pencil />
-            </div>
-            <div>
-                Square 
-            </div>
-            <div>
-                Circle  
-            </div>
-            <div>
-                Pen  
-            </div>
-            <div>
-                Eraser
-            </div>
+            <ToolButton 
+                label='Select'
+                icon={MousePointer2}
+                onClick={() => {}}
+                isActive={false}
+            />
+            <ToolButton 
+                label='Text'
+                icon={Type}
+                onClick={() => {}}
+                isActive={false}
+            />
+            <ToolButton 
+                label='Sticky Note'
+                icon={StickyNote}
+                onClick={() => {}}
+                isActive={false}
+            />
+            <ToolButton 
+                label='Rectangle'
+                icon={Square}
+                onClick={() => {}}
+                isActive={false}
+            />
+            <ToolButton 
+                label='Circle'
+                icon={Circle}
+                onClick={() => {}}
+                isActive={false}
+            />
+            <ToolButton 
+                label='Draw'
+                icon={Pencil}
+                onClick={() => {}}
+                isActive={false}
+            />
         </div>
         <div className='bg-white rounded-md p-1.5 flex flex-col shadow-md items-center'>
             <div>
