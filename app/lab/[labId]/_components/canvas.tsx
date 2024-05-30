cb5cc1aaf34ea743edcb10e3c9cf53742e45fcd2
AkshatJha21 <akshatj2134@gmail.com>
2024-05-30 10:39:02 +0530

    selection box init

--- app/lab/[labId]/_components/canvas.tsx
+++ app/lab/[labId]/_components/canvas.tsx
@@ -12,6 +12,7 @@ import { connectionIdToColor, pointerEventToCanvasPoint } from '@/lib/utils';
 import { nanoid } from "nanoid";
 import { LiveObject } from '@liveblocks/client';
 import { LayerPreview } from './layer-preview';
+import { SelectionBox } from './selection-box';
 
 const MAX_LAYERS = 100;
 
@@ -163,6 +164,7 @@ const Canvas = ({ labId }: CanvasProps) => {
                 selectionColor={layerIdsToColorSelection[layerId]}
               />
             ))}
+            <SelectionBox onResizeHandlePointerDown={() => {}}/>
             <CursorsPresence />
           </g>
         </svg>
---
+++ app/lab/[labId]/_components/selection-box.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { memo } from "react";
+
+interface SelectionBoxProps {
+    onResizeHandlePointerDown: () => void;
+};
+
+export const SelectionBox = memo(({ onResizeHandlePointerDown }: SelectionBoxProps) => {
+    return (
+        <div>
+
+        </div>
+    );
+});
+
+SelectionBox.displayName = "SelectionBox";
